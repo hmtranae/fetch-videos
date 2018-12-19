47951ca1035afa49fcbb3829dcab962f42cc02e3
Hieu Tran <hmtran.ae@gmail.com>
2018-12-19 14:45:24 -0500

    Render an VideoItem within VideoList

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,13 +4,28 @@ class SearchBar extends React.Component {
 
     state = { term: '' };
 
+    onInputChange = (e) => {
+        e.preventDefault();
+        this.setState({ term: e.target.value });
+    };
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        // TODO: Make sure we call callback from parent component
+        this.props.onFormSubmit(this.state.term);
+    };
+
     render() {
         return (
             <div className='search-bar ui segment'>
-                <form className='ui form'>
+                <form className='ui form' onSubmit={this.onFormSubmit}>
                     <div className='field'>
                         <label>Video Search</label>
-                        <input value={this.state.term} type='text'></input>
+                        <input
+                            value={this.state.term}
+                            type='text'
+                            onChange={this.onInputChange}
+                            />
                     </div>
                 </form>
             </div>
